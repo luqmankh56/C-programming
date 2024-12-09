b1a98544545c3ca4f5506c494bf615fa2ca8c038
Luqman Khan <158295061+luqmankh56@users.noreply.github.com>
2024-12-09 23:30:28 +0500

    Update Palindrome.c

--- Pointers/Palindrome.c
+++ Pointers/Palindrome.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 int main(){
-	
     char str[30];
-    int i, length, isPalindrome = 1;
+    char *start, *end;
 
-    printf("Enter string: ");
+
+    printf("Enter a string: ");
     scanf("%s", str);
 
-    length = strlen(str);
+    
+    start = str;  
+    end = str + strlen(str) - 1; 
 
-    for(i = 0; i < length / 2; i++){
-        if(str[i] != str[length - i - 1]){
-            isPalindrome = 0;
-            break;
+    
+    while (start < end) {
+        if (*start != *end) {
+            printf("Not a palindrome.\n");
+            return 0;  
         }
+        start++;
+        end--; 
     }
 
-    if(isPalindrome)
-        printf("Palindrome.\n");
-    else
-        printf("Not a palindrome.\n");
+    printf("Palindrome.\n");
 
     return 0;
 }
